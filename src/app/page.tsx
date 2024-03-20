f2363189665ad2d5273a2ed2d840db43473f07a5
TripleKdev <80637250+kangkengkhadev@users.noreply.github.com>
2024-03-20 23:06:50 +0700

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import Banner from '@/components/Banner'
-import Card from "@/components/Card";
-import styles from "./page.module.css";
-import CardPanel from "@/components/CardPanel";
+import PromoteCard from '@/components/PromoteCard';
 
 export default function Home() {
   return (
     <main>
       <Banner/>
+      <PromoteCard></PromoteCard>
     </main>
   );
 }
